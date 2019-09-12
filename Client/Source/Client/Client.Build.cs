d4f92f9bc4fdaa570634ac3476056bd71430e120
jeremyLee0512 <2410250284@qq.com>
2019-09-12 21:13:03 +0800

    客户端服务器都能用

--- Client/Source/Client/Client.Build.cs
+++ Client/Source/Client/Client.Build.cs
@@ -8,9 +8,9 @@ public class Client : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "KBEnginePlugins" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "KBEnginePlugins" });
+		//PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
