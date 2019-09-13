0e2e65a58a7e7025367ac079c5c1cb8746a2cb5e
jeremyLee0512 <2410250284@qq.com>
2019-09-13 09:22:14 +0800

    UMG_LoginMain

---
+++ Client/Source/Client/Public/RPGGameModeBase.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIENT_API ARPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	//virtual void BeginPlay() override;
+
+	//virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+};
